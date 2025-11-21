bfd38bbf98d9298226928e73fd47fc694dc6d120
Polaris-jkd <dhruvsaini7103@gmail.com>
2025-11-22 02:01:55 +0530

    Fix ThemeProvider wrapper and footer email display

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ThemeProvider } from './utils/useTheme.jsx';
 import App from './App.jsx';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './styles/global.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
+  </StrictMode>
 );
-
