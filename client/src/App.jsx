2c46dff82000a47f5c3015453416bf08a606fd66
Polaris-jkd <dhruvsaini7103@gmail.com>
2025-11-22 01:41:24 +0530

    Theme fixes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,27 @@
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
+import Footer from './components/Footer'; 
 import Home from './pages/Home';
 import Builder from './pages/Builder';
+import { ThemeProvider } from './hooks/useTheme.jsx';
 import './styles/global.css';
 
-export default function App() {
+function App() {
   return (
-    <Router>
-      <div style={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
-        <Navbar />
-        <main style={{ flex: 1 }}>
+    <ThemeProvider>
+      <Router>
+        <div className="app">
+          <Navbar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/builder" element={<Builder />} />
           </Routes>
-        </main>
-        <Footer />
-      </div>
-    </Router>
+          <Footer /> 
+        </div>
+      </Router>
+    </ThemeProvider>
   );
 }
+
+export default App;
